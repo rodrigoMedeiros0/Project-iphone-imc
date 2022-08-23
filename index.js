f480f89639fe03afbb51e80c8dba72132e16b86e
Rodrigo <rodrigovinicius.bsb@gmail.com>
2022-08-23 10:30:04 -0300

    add button css + javascript code

---
+++ index.js
@@ -0,0 +1,26 @@
+function calculateIMC() {
+
+    let peso = document.getElementById('peso').value;
+    let altura = document.getElementById('altura').value;
+
+    let imc = peso / (altura * altura);
+
+    if (imc >= 40) {
+        document.getElementById('message-imc').innerHTML = "Obesidade grave";
+    } else if (imc >= 30 ) {
+        document.getElementById('message-imc').innerHTML = "Obesidade";
+    } else if (imc >= 25 ) {
+        document.getElementById('message-imc').innerHTML = "Sobrepeso";
+    }else if (imc >= 18.5 ) {
+        document.getElementById('message-imc').innerHTML = "Peso ideal";
+    } else {
+        document.getElementById('message-imc').innerHTML = "Abaixo do peso";
+    }
+
+    document.getElementById('value-imc').innerHTML = imc.toFixed(2);
+}
+
+function reload() {
+
+    window.location.reload();
+}
